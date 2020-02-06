ac3d903ec7970a2acfa0d9b0ee8862b10e3818a2
Andrew Kalil <1541@holbertonschool.com>
2020-02-06 14:31:08 -0500

    commit 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
 
 /**
- *The "main" function will assign a random intiger to the variable n
- *of the int n. The function can give a positive or a negative number 
- *every time it was ran.
+ * main - Entry point
+ *
+ * Return: 0 (success)
  */
+/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	{
 if (n > 0)
+{
 printf("%d is positive\n", n);
+}
 else if (n == 0)
+{
 printf("%d is zero\n", n);
+}
 else if (n < 0)
+{
 printf("%d is negative\n", n);
-	}
+}
 	return (0);
 }
