2fd9496acfceefc3032be06f857acdfec9e3f19d
AndrewKalil <1541@holbertonschool.com>
2020-02-10 18:04:26 -0500

    commit 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - checks for lowercase or uppercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase or uppercase, 0 otherwise
+ */
+int _isalpha(int c)
+
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
