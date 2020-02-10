e9fcce70cffa7aaa5154a5d4a9f971765fd7079b
AndrewKalil <1541@holbertonschool.com>
2020-02-10 18:58:25 -0500

    commit 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @l: the int to extract the last digit from
+ * Return: value of the last digit
+ */
+int print_last_digit(int l)
+{
+	int a;
+
+	if (l < 0)
+	{
+		l = -l;
+	}
+	a = l % 10;
+	if (a < 0)
+	{
+		a = -a;
+	}
+	_putchar (a + '0');
+	return (a);
+}
