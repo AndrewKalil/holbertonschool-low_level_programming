68fca60690013084c517393f131f0cc58ca404ad
AndrewKalil <1541@holbertonschool.com>
2020-02-11 03:45:24 -0500

    commit 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - two integers and return the result
+ * @a: int that will be added to k
+ * @k: int that will be added to a
+ * Return: sum of a and k
+ */
+int add(int a, int k)
+{
+	int o;
+
+	o = a + k;
+
+	return (o);
+
+}
