6749f81b9b92167b894ef73717668f905fe45fce
AndrewKalil <1541@holbertonschool.com>
2020-02-13 12:22:04 -0500

    replacing numbers with words

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints the word fizz for multiples of 3, the word
+ * buzz for multiples of 5 and fizzbuzz for multiples of both 3
+ * 3 and 5
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		else if (a % 3 != 0 && a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (a == 1)
+		{
+			printf("%d ", a);
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
