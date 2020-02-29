a82c9526a63c110d3bccbca86005d3e0c159a7ca
AndrewKalil <1541@holbertonschool.com>
2020-02-29 12:35:57 -0500

    commit 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints name of program
+ * @argc: number of arguments
+ * @argv: array of string of arguments
+ *
+ * Return: alays 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
