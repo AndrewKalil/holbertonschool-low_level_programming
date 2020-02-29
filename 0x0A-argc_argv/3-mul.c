7a8cfe94d44832543a2d936d71de506cf1ccb081
AndrewKalil <1541@holbertonschool.com>
2020-02-29 13:46:55 -0500

    commit 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -55,8 +55,8 @@ int main(int argc, char *argv[])
 	int num2;
 	int product;
 
-	num1 = _atoi(*(argv + 1));
-	num2 = _atoi(*(argv + 2));
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
 	product = num1 * num2;
 
 	if (argc == 3)
