a5d1d822c4a5401c3239ab75cf154ee7696fa44d
AndrewKalil <1541@holbertonschool.com>
2020-03-03 15:18:29 -0500

    commit 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * free_grid - frees a 2 dimensional array
+ * @grid: two dimensional array
+ * @height: one dimensional array
+ */
 void free_grid(int **grid, int height)
 {
 	int i;
@@ -11,9 +16,9 @@ void free_grid(int **grid, int height)
 
 	i = 0;
 	while (i < height)
-		{
-			free(grid[i]);
-			i++;
-		}
+	{
+		free(grid[i]);
+		i++;
+	}
 	free(grid);
 }
