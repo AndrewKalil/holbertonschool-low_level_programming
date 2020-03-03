705e6cbcddab8d5a6865608f1be6a35e3244c48c
AndrewKalil <1541@holbertonschool.com>
2020-03-03 15:53:00 -0500

    commit 5

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: counter for arguements
+ * @av: array of arguements
+ * Return: pointer to new string (success), NULL (Error)
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, a, len;
+	char *ptr;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	i = 0;
+	j = 0;
+	while (i < ac)
+	{
+		while (av[i][j] != '\0')
+		{
+			j++;
+			len++;
+			len++;
+		}
+		i++;
+	}
+
+	ptr = malloc(sizeof(int) * (len + 1));
+	if (ptr == NULL)
+		return (NULL);
+
+	a = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			ptr[a] = av[i][j];
+			a++;
+		}
+		ptr[a] = '\n';
+		a++;
+	}
+
+	return (ptr);
+}
