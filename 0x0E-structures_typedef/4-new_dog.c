e781b33974b73b717ffa5f170ade40093f44a028
AndrewKalil <1541@holbertonschool.com>
2020-03-09 14:48:28 -0500

    changed condition after mallocs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -51,21 +51,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
-	{
-		free(ptr);
 		return (NULL);
-	}
 	ptr->name = malloc(_strlen(name) + 1);
 	if (ptr->name == NULL)
 	{
-		free(ptr->name);
 		free(ptr);
 		return (NULL);
 	}
 	ptr->owner = malloc(_strlen(owner) + 1);
 	if (ptr->owner == NULL)
 	{
-		free(ptr->owner);
+		free(ptr->name);
 		free(ptr);
 		return (NULL);
 	}
