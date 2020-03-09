3cb11a4ec364b043aada4b88af25a2be630ceaf1
AndrewKalil <1541@holbertonschool.com>
2020-03-09 14:37:42 -0500

    used malloc to allocate structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -59,12 +59,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (ptr->name == NULL)
 	{
 		free(ptr->name);
+		free(ptr);
 		return (NULL);
 	}
 	ptr->owner = malloc(_strlen(owner));
 	if (ptr->owner == NULL)
 	{
 		free(ptr->owner);
+		free(ptr);
 		return (NULL);
 	}
 
