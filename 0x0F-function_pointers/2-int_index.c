fae2a5a8a32823e982561fa1d684c7c761f85a61
AndrewKalil <1541@holbertonschool.com>
2020-03-11 13:48:33 -0500

    fixed return value

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,9 +3,11 @@
 #include "function_pointers.h"
 
 /**
- *
- *
- *
+ * int_index - searches for integer
+ * @array: array of integers
+ * @size: number of elements in the array
+ * @cmp: function that searches for integer
+ * Return: i which is the index or -1 if not found
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
