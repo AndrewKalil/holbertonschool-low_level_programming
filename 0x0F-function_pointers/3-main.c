2f5ada123bfa6b5e768623cbf5893d74ad06be4a
AndrewKalil <1541@holbertonschool.com>
2020-03-11 18:51:01 -0500

    added description

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+
+/**
+ * main - calls functions for calculator
+ * @argc: number of arguements
+ * @argv: arguements
+ * Return: a number as result
+ *
+ */
+int main(int argc, char **argv)
+{;
+	int a, b, result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	result = get_op_func(argv[2])(a, b);
+
+	printf("%d\n", result);
+
+	return (0);
+
+}
