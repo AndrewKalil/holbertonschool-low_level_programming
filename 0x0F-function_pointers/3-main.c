9d5c744cea2fd40453167986b03aacced2530e81
AndrewKalil <1541@holbertonschool.com>
2020-03-11 19:01:34 -0500

    fixed compilation error

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,28 +7,26 @@
  * Return: a number as result
  *
  */
-int main(int argc, char **argv)
-{;
-	int a, b, result;
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	result = get_op_func(argv[2])(a, b);
-
+int main(int argc, char *argv[])
+{
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	
-	printf("%d\n", result);
-
+	else
+	{
+		printf("%i\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	}
 	return (0);
-
 }
