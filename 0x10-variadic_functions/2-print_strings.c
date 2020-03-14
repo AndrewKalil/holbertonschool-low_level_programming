c38e2fb8c250a44b9b50c97ace6b91d1de2b07aa
AndrewKalil <1541@holbertonschool.com>
2020-03-14 13:00:56 -0500

    betty

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,7 +1,9 @@
 #include "variadic_functions.h"
 
 /**
- *
+ * print_strings - prints strings passed as arguements
+ * @separator: string of comman and space printed after a number
+ * @n: number of arguemnts passed
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
@@ -9,19 +11,19 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	char *s;
 
-	va_start (ap, n);
+	va_start(ap, n);
 	while (i < n)
 	{
 		s = va_arg(ap, char*);
 		if (s == NULL)
 			s = "(nil)";
 		if ((separator == 0) || (separator != 0 && i == 0))
-			printf ("%s", s);
+			printf("%s", s);
 		else
-			printf ("%s%s", separator, s);
+			printf("%s%s", separator, s);
 		i++;
 	}
 	va_end(ap);
 
-	printf ("\n");
+	printf("\n");
 }
