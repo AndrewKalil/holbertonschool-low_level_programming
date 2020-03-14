adab2f8062aed876eb72adc109257b0ed7d41e1d
AndrewKalil <1541@holbertonschool.com>
2020-03-14 12:42:19 -0500

    fixed conditions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+
+/**
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *s, *separator = "";
+	va_list ap;
+
+	va_start(ap, format);
+
+	if (format != NULL)
+	{
+		while (format[i] != '\0')
+		{
+			switch (format[i])
+			{
+			case 'c':
+				printf ("%s%c", separator, va_arg(ap, int));
+				break;
+			case 'i':
+				printf ("%s%i", separator, va_arg(ap, int));
+				break;
+			case 'f':
+				printf ("%s%f", separator, va_arg(ap, double));
+				break;
+			case 's':
+				s = va_arg(ap, char *);
+				if (s == NULL)
+					s = "(nil)";
+				printf ("%s%s", separator, s);
+				break;
+			default:
+				i++;
+				continue;
+			}
+			separator = ", ";
+			i++;
+		}
+	}
+	va_end(ap);
+	printf ("\n");
+}
