778105b687be5db5710a35bd0c88d3392a9afd63
AndrewKalil <1541@holbertonschool.com>
2020-03-14 13:00:46 -0500

    fixed betty

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,23 +1,25 @@
 #include "variadic_functions.h"
 
 /**
- *
+ * print_numbers - prints integers passes as arguents
+ * @separator: string containing comman and space
+ * @n: number of arguements being passed
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i = 0;
 
-	va_start (ap, n);
+	va_start(ap, n);
 	while (i < n)
 	{
 		if ((separator == 0) || (separator != 0 && i == 0))
-			printf ("%d", va_arg(ap, int));
+			printf("%d", va_arg(ap, int));
 		else
-			printf ("%s%d", separator, va_arg(ap, int));
+			printf("%s%d", separator, va_arg(ap, int));
 		i++;
 	}
 	va_end(ap);
 
-	printf ("\n");
+	printf("\n");
 }
