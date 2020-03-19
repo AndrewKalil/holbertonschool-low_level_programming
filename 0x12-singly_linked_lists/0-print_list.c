f859ef76e0ff435588ba7de3d2cd39642dc82f08
AndrewKalil <1541@holbertonschool.com>
2020-03-19 15:03:48 -0500

    removed parenthesis and fixed betty problems

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,10 @@
 #include "lists.h"
 
 /**
- *
+ * print_list - prints elements in the list and returns number of
+ * elements
+ * @h: pointer to struct
+ * Return: number of elements in struct
  */
 size_t print_list(const list_t *h)
 {
@@ -12,7 +15,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nill)\n");
 		else
-			printf("[%u] (%s)\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
