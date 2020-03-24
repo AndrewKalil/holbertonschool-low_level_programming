f6600c3fdb96f224665ded99e826adf25f46dccd
AndrewKalil <1541@holbertonschool.com>
2020-03-24 09:58:24 -0500

    added condtion

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,8 +27,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(tmp);
 		return (1);
 	}
-	for (i = 0; (tmp || tmp->next) && i < index - 1; i++)
+	for (i = 0; i < index - 1; i++)
 	{
+		if (!(tmp || tmp->next))
+			return (-1);
+
 		tmp = tmp->next;
 	}
 	atm = tmp->next;
