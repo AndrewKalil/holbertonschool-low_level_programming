e24c16f1dfde07204a6263432135a0ca21f1e36a
AndrewKalil <1541@holbertonschool.com>
2020-03-24 09:25:55 -0500

    fixed betty

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,7 +4,7 @@
 #include "lists.h"
 
 /**
- * get_node_at_index - returns nth node of the list
+ * get_nodeint_at_index - returns nth node of the list
  * @head: pointer to node
  * @index: number of node identifier
  * Return: pointer to node position
@@ -18,18 +18,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head == NULL)
 		return (0);
-	else
+	for (i = 0; i < index; i++)
 	{
-		for (i = 0; i < index; i++)
-		{
-			if (head == NULL)
-				return (NULL);
-			else
-			{
-				pos = head->next;
-				head = pos;
-			}
-		}
+		if (head == NULL)
+			return (NULL);
+
+		pos = head->next;
+		head = pos;
 	}
 	return (pos);
 }
