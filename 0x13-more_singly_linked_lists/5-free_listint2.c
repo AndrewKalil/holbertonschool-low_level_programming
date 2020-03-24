1beea78b097f39fc4cd95b114f0754b1d9dfcd80
AndrewKalil <1541@holbertonschool.com>
2020-03-24 09:49:22 -0500

    fixed condition

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,14 +11,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (head == NULL)
+		return;
+
 	while (*head)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 	}
-	if (head == NULL)
-		return;
-
 	*head = NULL;
 }
