89ee4715ec6836676e99288f247433d471cbbf7d
AndrewKalil <1541@holbertonschool.com>
2020-03-25 11:20:46 -0500

    NULL condition

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ *reverse_listint - reverse list
+ *@head: head of the list to be reversed
+ *Return: return the reversed list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *previous, *next;
+
+	previous = NULL;
+
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (*head != NULL)
+	{
+		next =  (*head)->next;
+		(*head)->next = previous;
+		previous = *head;
+		*head = next;
+	}
+
+	*head = previous;
+	return (previous);
+
+}
