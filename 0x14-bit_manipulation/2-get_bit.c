9140b65c7bb204dedc9c2351e2b28a52901cc632
AndrewKalil <1541@holbertonschool.com>
2020-03-26 16:20:50 -0500

    fixed prototype

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * get_bit - returns the value of a bit at the position indicated by
+ * position at indicated by the index
+ * @n: number to be scaned
+ * @index: index of the bit
+ * Return: value of bit (1 or 0)
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+
+	return ((n & (1 << index)) != 0);
+}
