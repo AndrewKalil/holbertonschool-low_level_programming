b99a7fbc601e8c33d97d3506c5dc5a29add6233a
AndrewKalil <1541@holbertonschool.com>
2020-03-26 15:31:38 -0500

    changed putchar

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,14 +9,14 @@ void print_binary(unsigned long int n)
 {
 	if (n == 0)
 	{
-		_putchar('0');
+		putchar('0');
 		return;
 	}
 	else if (n == 1)
 	{
-		_putchar('1');
+		putchar('1');
 		return;
 	}
 	print_binary(n >> 1);
-	_putchar('0' + (n & 1));
+	putchar((n & 1) + 48);
 }
