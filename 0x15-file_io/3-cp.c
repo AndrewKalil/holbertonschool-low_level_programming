6e09ef1302fecd4c0e33adbc51cd7858b2a8cc22
AndrewKalil <1541@holbertonschool.com>
2020-03-31 17:49:03 -0500

    first commit

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,56 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	char buf[BUFSIZ];
+	int num, err1, err2;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((num = read(fd_from, buf, BUFSIZ)) > 0)
+	{
+		if (fd_to < 0 || write(fd_to, buf, num) != num)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_from);
+			exit(99);
+		}
+	}
+	if (num < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	err1 = close(fd_from);
+	err2 = close(fd_to);
+	if (err1 < 0 || err2 < 0)
+	{
+		if (err1 < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		if (err2 < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+	return (0);
+}
