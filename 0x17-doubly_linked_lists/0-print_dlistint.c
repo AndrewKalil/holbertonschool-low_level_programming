ff776ea9662ac22104f57323a24be407b09a8e2f
AndrewKalil <1541@holbertonschool.com>
2020-04-30 15:04:51 -0500

    adding first task

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,18 @@
 #include "lists.h"
 
 /**
- *
+ * print_dlistint - prints elements in a list and returns number of elemenets
+ * @h: pointer to the head
+ * Return: number of nodes
  */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
