becf9e6e649782d3298703c544a010b77de602c4
AndrewKalil <1541@holbertonschool.com>
2020-04-30 16:24:32 -0500

    adding the fourth task

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,5 +1,37 @@
 #include "lists.h"
 
 /**
- *
+ * add_dnodeint - adds a node at the beginneing of the list
+ * @head: pointer to a pointer to the head
+ * @n: int variable containing data to be stored
+ * Return: pointer to the new node
  */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *h = *head;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	if (h == NULL)
+	{
+		new->n = n;
+		new->next = NULL;
+		new->prev = NULL;
+		*head = new;
+	}
+	else
+	{
+		while (h->next != NULL)
+		{
+			h = h->next;
+		}
+		new->n = n;
+		new->prev = h;
+		h->next = new;
+		new->next = NULL;
+	}
+
+	return (new);
+}
