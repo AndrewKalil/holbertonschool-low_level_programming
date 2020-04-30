4fa90eca1a65902b0fcf4fbad5f46ea775d2177c
AndrewKalil <1541@holbertonschool.com>
2020-04-30 17:08:15 -0500

    adding 6th task

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,23 @@
 #include "lists.h"
 
 /**
- *
+ * get_dnodeint_at_index - returns the nth node
+ * @head: pointer to the head
+ * @index: position of node to be returned
+ * Return: pointer to the node to be returned
  */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *node = head;
+
+	if (head == NULL)
+		return (NULL);
+	while (node != NULL && i < index)
+	{
+		node = node->next;
+		i++;
+	}
+
+	return (node);
+}
