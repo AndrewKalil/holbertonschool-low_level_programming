05f18e7c116ec0592d1fb4504fee7da6ec8e21bb
AndrewKalil <1541@holbertonschool.com>
2020-04-30 16:52:32 -0500

    adding the 5th task

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,20 @@
 #include "lists.h"
 
 /**
- *
+ * free_dlistint - adds a node at the beginneing of the list
+ * @head: pointer to a pointer to the head
+ * Return: pointer to the new node
  */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *h;
+
+	h = head;
+	while (head != NULL)
+	{
+		head = head->next;
+		free(h);
+		h = head;
+	}
+	free(head);
+}
