a22abeeef303799f6bf25f714da903582f95438f
AndrewKalil <1541@holbertonschool.com>
2020-04-30 15:44:55 -0500

    betty fixes and third task

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,18 @@
 #include "lists.h"
 
 /**
- *
+ * dlistint_len - iterates throught the list and returns the number of nodes
+ * @h: pointer to the head of the list
+ * Return: number of elements
  */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,5 +1,27 @@
 #include "lists.h"
 
 /**
- *
+ * add_dnodeint - adds a node at the beginneing of the list
+ * @head: pointer to a pointer to the head
+ * @n: int variable containing data to be stored
+ * Return: pointer to the new node
  */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	if (*head)
+		(*head)->prev = new;
+	*head = new;
+
+	return (new);
+}
