b3926e2c22be8dd6e643708cc3b6bfbfb99f80b9
AndrewKalil <1541@holbertonschool.com>
2020-05-02 01:35:21 -0500

    NULL h condition

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,11 +23,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		ptr = ptr->next;
 		i++;
-		if (ptr == NULL)
-		{
-			free(ptr);
-			return (NULL);
-		}
 	}
 	if (ptr != NULL)
 	{
