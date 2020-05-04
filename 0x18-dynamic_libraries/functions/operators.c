c793873de6803bb7b03b48a373e5362cb0db4351
AndrewKalil <1541@holbertonschool.com>
2020-05-04 08:28:51 -0500

    first advanced

---
+++ 0x18-dynamic_libraries/functions/operators.c
@@ -0,0 +1,57 @@
+#include "operators.h"
+#include <stdio.h>
+
+/**
+ * add - returns the sum of two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - returns the differences between two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - returns the product of two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - returns the quotient of two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - returns the remainder of dividing two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
