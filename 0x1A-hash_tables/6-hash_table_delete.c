604bea0133ce280edef890b22112c990454aef60
AndrewKalil <1541@holbertonschool.com>
2020-05-29 10:43:29 -0500

    fixing task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -17,14 +17,14 @@ void hash_table_delete(hash_table_t *ht)
 		while (ht->array[i])
 		{
 			ptr = ht->array[i]->next;
-            free(ht->array[i]->key);
-            free(ht->array[i]->value);
-            free(ht->array[i]);
-            ht->array[i] = ptr;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = ptr;
 		}
 	}
-    free(ht->array);
+	free(ht->array);
 	ht->array = NULL;
 	ht->size = 0;
-    free(ht);
+	free(ht);
 }
