3953a5f8f40d1211f64020efc41b0ca0707940ae
AndrewKalil <1541@holbertonschool.com>
2020-07-03 12:41:25 -0500

    pep8 for task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
+
 def island_perimeter(grid):
-	count = 0
-	for i in range(len(grid)):
-		for j in range(len(grid[i])):
-			if grid[i][j] == 1:
-				if grid[i][j+1] == 0:
-					count += 1
-				if grid[i][j-1] == 0:
-					count += 1
-				if grid[i+1][j] == 0:
-					count += 1
-				if grid[i-1][j] == 0:
-					count += 1
-	return count
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if grid[i][j+1] == 0:
+                    count += 1
+                if grid[i][j-1] == 0:
+                    count += 1
+                if grid[i+1][j] == 0:
+                    count += 1
+                if grid[i-1][j] == 0:
+                    count += 1
+    return count
