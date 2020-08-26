6638f2ba1d779f7a342fd8adcdfae2d30953ce89
AndrewKalil <1541@holbertonschool.com>
2020-08-26 15:30:17 -0500

    addinng all files and task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search for a value in an array
+ * @array: pointer to the first value in the array
+ * @size: size of the array
+ * @value: value being searched
+ * Return: the number being searched or -1 if not found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	int found = 0;
+
+	if (size == 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			found = 1;
+			break;
+		}
+	}
+	if (found == 1)
+		return (i);
+	else
+		return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,13 @@
+# Search Algorithms
+
+## Files
+* 0-linear.c
+* 1-binary.c
+* 2-O
+* 3-O
+* 4-O
+* 5-O
+* 6-O
+
+## Author
+Andrew Kalil
